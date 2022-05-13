7012164a66fe67a8b7e71104416432820b7f25c2
dolev146 <dolev146@gmail.com>
2022-05-13 16:58:41 +0300

    hay2

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -2,16 +2,11 @@
 using namespace std;
 namespace ariel
 {
-    OrgChart &OrgChart::add_root(string txt)
-    {
-        root.txt = txt;
-        return *this;
-    }
+
     OrgChart &OrgChart::add_sub(string parent, string child)
     {
         return *this;
     }
- 
 
     string *OrgChart::begin_level_order()
     {
@@ -52,16 +47,18 @@ namespace ariel
         // add node to test
         test.push_back(node.txt);
     }
+
+    OrgChart &OrgChart::add_root(string txt)
+    {
+        return *this;
+    }
+
     string *OrgChart::end_preorder()
     {
         return &test[test.size()];
     }
     ostream &operator<<(ostream &os, OrgChart &org)
     {
-        for (auto it = org.begin_preorder(); it != org.end_preorder(); ++it)
-        {
-            os << (*it) << ":-";
-        }
         return os;
     }
 
