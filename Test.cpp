d03d6cc7e335e25a973ab97dd7a9cb9f9e382913
dolev146 <dolev146@gmail.com>
2022-05-25 09:41:25 +0300

    finished testtsss

--- Test.cpp
+++ Test.cpp
@@ -103,7 +103,88 @@ TEST_CASE("Check not throw")
 
 TEST_CASE("Tree good valaues check")
 {
-    
+    OrgChart organization;
+    organization.add_root("CEO")
+        .add_sub("CEO", "CTO")    // Now the CTO is subordinate to the CEO
+        .add_sub("CEO", "CFO")    // Now the CFO is subordinate to the CEO
+        .add_sub("CEO", "COO")    // Now the COO is subordinate to the CEO
+        .add_sub("CTO", "VP_SW")  // Now the VP Software is subordinate to the CTO
+        .add_sub("COO", "VP_BI"); // Now the VP_BI is subordinate to the COO
 
-    
+    CHECK_NOTHROW(cout << organization << endl); /* Prints the org chart in a reasonable format. For example:
+         CEO
+         |--------|--------|
+         CTO      CFO      COO
+         |                 |
+         VP_SW             VP_BI
+   */
+    vector<string> string_iter;
+    for (auto it = organization.begin_level_order(); it != organization.end_level_order(); ++it)
+    {
+        string_iter.push_back(*it);
+    } // prints: CEO CTO CFO COO VP_SW VP_BI
+
+    CHECK_EQ(string_iter.at(0), "CEO");
+    CHECK_EQ(string_iter.at(1), "CTO");
+    CHECK_EQ(string_iter.at(2), "CFO");
+    CHECK_EQ(string_iter.at(3), "COO");
+    CHECK_EQ(string_iter.at(4), "VP_SW");
+    CHECK_EQ(string_iter.at(5), "VP_BI");
+
+    vector<string> string_iter_rev;
+
+    for (auto it = organization.begin_reverse_order(); it != organization.reverse_order(); ++it)
+    {
+        string_iter_rev.push_back(*it);
+    } // prints: VP_SW VP_BI CTO CFO COO CEO
+
+    CHECK_EQ(string_iter_rev.at(0), "VP_SW");
+    CHECK_EQ(string_iter_rev.at(1), "VP_BI");
+    CHECK_EQ(string_iter_rev.at(2), "CTO");
+    CHECK_EQ(string_iter_rev.at(3), "CFO");
+    CHECK_EQ(string_iter_rev.at(4), "COO");
+    CHECK_EQ(string_iter_rev.at(5), "CEO");
+
+    vector<string> string_iter_pre;
+    for (auto it = organization.begin_preorder(); it != organization.end_preorder(); ++it)
+    {
+        string_iter_pre.push_back(*it);
+    } // prints: CEO CTO VP_SW CFO COO VP_BI
+
+    CHECK_EQ(string_iter_pre.at(0), "CEO");
+    CHECK_EQ(string_iter_pre.at(1), "CTO");
+    CHECK_EQ(string_iter_pre.at(2), "VP_SW");
+    CHECK_EQ(string_iter_pre.at(3), "CFO");
+    CHECK_EQ(string_iter_pre.at(4), "COO");
+    CHECK_EQ(string_iter_pre.at(5), "VP_BI");
+
+    vector<string> string_iter_regular;
+    for (auto element : organization)
+    {
+        // this should work like level order
+        string_iter_regular.push_back(element);
+        cout << element << " ";
+    } // prints: CEO CTO CFO COO VP_SW VP_BI
+
+    CHECK_EQ(string_iter_regular.at(0), "CEO");
+    CHECK_EQ(string_iter_regular.at(1), "CTO");
+    CHECK_EQ(string_iter_regular.at(2), "CFO");
+    CHECK_EQ(string_iter_regular.at(3), "COO");
+    CHECK_EQ(string_iter_regular.at(4), "VP_SW");
+    CHECK_EQ(string_iter_regular.at(5), "VP_BI");
+
+    vector<size_t> int_iter_check_size;
+    // demonstrate the arrow operator:
+    for (auto it = organization.begin_level_order(); it != organization.end_level_order(); ++it)
+    {
+        int_iter_check_size.push_back(it->size());
+        cout << it->size() << " ";
+    } // prints: 3 3 3 3 5 5
+
+    CHECK_EQ(int_iter_check_size.at(0), 3);
+    CHECK_EQ(int_iter_check_size.at(1), 3);
+    CHECK_EQ(int_iter_check_size.at(2), 3);
+    CHECK_EQ(int_iter_check_size.at(3), 3);
+    CHECK_EQ(int_iter_check_size.at(4), 5);
+    CHECK_EQ(int_iter_check_size.at(5), 5);
 }
